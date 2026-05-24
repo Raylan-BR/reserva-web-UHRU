6e56ec9861916adb2c780fab539b8e06da230173
Raylan Bruno <raylanbruno2002@gmail.com>
2026-05-24 19:17:58 -0300

    config: logic nav load

--- static/script/index.js
+++ static/script/index.js
@@ -1,3 +1,15 @@
+// Mostrar tela do usuário autenticado ou não
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('/')
+    const token = localStorage.getItem('token');
+    fetch('/nav',{
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+        method: 'GET',
+    })
+    .then(response => response.text())
+    .then(html => {
+        document.querySelector('nav').innerHTML = html;
+    });
 });
