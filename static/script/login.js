2e29f3d658487d83cbce1f9b5c79468647ab328c
Raylan Bruno <raylanbruno2002@gmail.com>
2026-05-24 19:42:07 -0300

    config: logic login

---
+++ static/script/login.js
@@ -0,0 +1,26 @@
+document.querySelector('form').addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const email = document.querySelector('#login_email').value;
+
+    try {
+        fetch('/login',{
+            headers: {'Content-Type': 'application/json'},
+            method: 'POST',
+            body: JSON.stringify({'email': email}),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(`data: ${data}`)
+            if (data.token){
+                localStorage.setItem('token', data.token);
+                window.location.href = '/';
+            } else {
+                console.log('Não encontramos seu email !');
+            }
+        });
+    
+    } catch (error) {
+        console.error('Erro de requisição:', error);
+    }
+});
